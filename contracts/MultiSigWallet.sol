4a14c9e4dda0c47febea1b3f49476a03de51b39d
unknown <adityawork0811@gmail.com>
2025-04-24 18:29:05 +0530

    add MultiSigWallet contract implementation

---
+++ contracts/MultiSigWallet.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/* Multi Sig Wallet
+    Topics
+
+    1. event
+    2. array
+    3. mapping
+    4. struct
+    5. constructor
+    6. error
+    7. for-loop
+    8. fallback and payable
+    9. function modifier
+    10. call
+    11. view function
+
+    The wallet owners can:
+
+    1. submit a transaction.
+    2. approve and revoke approval of pending transactions.
+    3. anyone can execute a transaction after enough owners have approved it.
+ */
+
+contract MultiSigWallet {
+    event deposit(address indexed sender, uint256 amount, uint256 balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint256 indexed txIndex,
+        address indexed to,
+        uint256 value,
+        bytes data
+    );
+    event ConfirmTransacion(address indexed owner, uint256 indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint256 indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint256 public numConfirmationsRequired;
+
+    struct Transaction {
+        address To;
+        uint256 value;
+        bytes data;
+        bool executed;
+        uint256 numConfirmations;
+    }
+
+    // mapping from tx index => owner => bool
+    mapping(uint256 => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] public transactions;
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+
+    modifier txExists(uint256 _txIndex) {
+        require(_txIndex < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notExecuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
+
+    modifier notConfirmed(uint256 _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "tx already confirmed");
+        _;
+    }
+}
